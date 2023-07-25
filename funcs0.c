da17f76839da5b2020aaf5f31378f2318796f695
Okwaro99 <133528154+Okwaro99@users.noreply.github.com>
2023-07-25 19:12:35 +0300

    Update funcs0.c

--- funcs0.c
+++ funcs0.c
@@ -3,10 +3,10 @@
 /************************* PRINT CHAR *************************/
 
 /**
- * print_char - Prints a char
- * @types: List a of arguments
+ * print_char - Prints a char.
+ * @types: List a of arguments.
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculates active flags.
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
